2e9a247c74f6fdfe07d76efc2faf42e687ddd6a1
fictiv75 <fictiv75@gmail.com>
2024-07-27 21:41:05 +0300

    finish make header

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,12 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+const seacrhIcon = document.querySelector(".search-form__icon");
+const searchBody = document.querySelector(".search-form__body");
+if (seacrhIcon) {
+  seacrhIcon.addEventListener("click", function (e) {
+    seacrhIcon.classList.toggle("active");
+    searchBody.classList.toggle("active");
+  });
+}
